44da97f9beeb1f484079b00841c65aa6a860d473
Fausto Stangler <fausto.stangler@gmail.com>
2023-03-16 11:35:43 -0300

    heroku unicorn

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-from dash import Dash, dcc, html, Input, Output
-import os
+from dash import Dash, callback, html, dcc
+import dash_bootstrap_components as dbc
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does. I.e. This is your HTTP server
+from whitenoise import WhiteNoise   #for serving static files on Heroku
 
+# Instantiate dash app
+app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# Reference the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
+server = app.server 
 
-app = Dash(__name__, external_stylesheets=external_stylesheets)
+# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
+server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
-server = app.server
+# Define Dash layout
+def create_dash_layout(app):
 
-app.layout = html.Div([
-    html.H1('Hello World'),
-    dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
-    ),
-    html.Div(id='display-value')
-])
+    # Set browser tab title
+    app.title = "Your app title" 
+    
+    # Header
+    header = html.Div([html.Br(), dcc.Markdown(""" # Hi. I'm your Dash app."""), html.Br()])
+    
+    # Body 
+    body = html.Div([dcc.Markdown(""" ## I'm ready to serve static files on Heroku. Just look at this! """), html.Br(), html.Img(src='charlie.png')])
 
-@app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
-def display_value(value):
-    return f'You have selected {value}'
+    # Footer
+    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built with ![Image](heart.png) in Python using [Dash](https://plotly.com/dash/)""")])
+    
+    # Assemble dash layout 
+    app.layout = html.Div([header, body, footer])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    return app
+
+# Construct the dash layout
+create_dash_layout(app)
+
+# Run flask app
+if __name__ == "__main__": app.run_server(debug=False, host='0.0.0.0', port=8050)
