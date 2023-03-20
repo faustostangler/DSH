4b74f2d39f7a220d5733f5878e2e95193429518e
Fausto Stangler <fausto.stangler@gmail.com>
2023-03-20 17:21:59 -0300

    update_world_markets

--- periodic_task.py
+++ periodic_task.py
@@ -1,8 +1,11 @@
 import assets.helper as b3
 
 def my_periodic_task():
-    value = 'update_b3_companies'
-    value = b3.update_b3_companies(value)
+    # value = 'update_b3_companies'
+    # value = b3.update_b3_companies(value)
+
+    value = 'world_markets'
+    value = b3.update_world_markets(value)
     print("All updated!")
 
 if __name__ == "__main__":
