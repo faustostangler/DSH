9b1e1377dd21494dc0771a37bdad7ca89f4a5c45
Fausto Stangler <fausto.stangler@gmail.com>
2023-10-12 19:11:21 -0300

    subsetor

--- pages/setor.py
+++ pages/setor.py
@@ -3,24 +3,38 @@ from dash.dependencies import Input, Output
 
 from app import app
 
+# ----- LAYOUT -----
 layout = html.Div([
+    # Title of the page
     html.H1("Análise Fundamentalista"),
+    
+    # Subtitle that will be updated based on user selection
     html.H2(id='setor-title'),
+    
+    # Uncomment below lines to include additional content as needed.
+    # Example: A graph that might be used to display some data.
     # dcc.Graph(id='graph-ativos'),
-    # Your contents here...
+    
+    # Additional contents here...
+    # Include other HTML components and Dash components as per the design requirements.
 ])
 
-# Callbacks for this page...
-# update the content based on the selected 'setor'
+# ----- CALLBACKS -----
+# Callback to update the content based on the selected 'setor'
 @app.callback(
-    Output('setor-title', 'children'),
-    Input('store-selected-setor', 'data')
+    Output('setor-title', 'children'),  # Target component where updated data will be displayed
+    Input('store-selected-setor', 'data')  # Source component which triggers the callback
 )
 def update_setor_content(stored_setor):
+    # Check if there's any data stored, if not, prevent the update
     if stored_setor is None:
         raise exceptions.PreventUpdate
     
+    # Retrieve 'setor' value from the stored data
     setor = stored_setor.get('setor', '')
+    
+    # Update the title based on the selected 'setor'
+    # If 'setor' is available, display it as the subtitle; otherwise, display an empty string
     if setor:
         return f"{setor}"
     else:
--- pages/subsetor.py
+++ pages/subsetor.py
@@ -3,28 +3,50 @@ from dash.dependencies import Input, Output
 
 from app import app
 
+# ----- LAYOUT -----
 layout = html.Div([
+    # Main title of the page
     html.H1("Análise Fundamentalista"),
+    
+    # Subtitle that will be updated based on user selection
     html.H2(id='subsetor-title'),
+    
+    # Additional contents might go here, like graphs, tables, etc.
+    # Uncomment and adjust the following line as per your requirements.
     # dcc.Graph(id='graph-ativos'),
-    # Your contents here...
+    
+    # More contents here...
 ])
 
-# Callbacks for this page...
-# update the content based on the selected 'setor' and 'subsetor'
+# ----- CALLBACKS -----
+# Callback to update the content based on the selected 'setor' and 'subsetor'
 @app.callback(
-    Output('subsetor-title', 'children'),
-    [Input('store-selected-setor', 'data'),
-     Input('store-selected-subsetor', 'data')]
+    Output('subsetor-title', 'children'),  # Target component to display the updated data
+    [Input('store-selected-setor', 'data'),  # Source component which triggers the callback
+     Input('store-selected-subsetor', 'data')]  # Another source component for the callback
 )
 def update_subsetor_content(stored_setor, stored_subsetor):
+    """
+    Update the subtitle based on selected 'setor' and 'subsetor'.
+
+    Parameters:
+    - stored_setor (dict): Stored data for selected 'setor'.
+    - stored_subsetor (dict): Stored data for selected 'subsetor'.
+
+    Returns:
+    - str: Updated subtitle string containing 'setor' and 'subsetor'.
+    """
+    # Check if there's any data stored, if not, prevent the update
     if stored_setor is None or stored_subsetor is None:
         raise exceptions.PreventUpdate
     
+    # Retrieve 'setor' and 'subsetor' values from the stored data
     setor = stored_setor.get('setor', '')
     subsetor = stored_subsetor.get('subsetor', '')
+    
+    # Update the title based on the selected 'setor' and 'subsetor'
+    # If 'subsetor' is available, display it along with 'setor' as the subtitle; otherwise, display an empty string
     if subsetor:
         return f"{setor} - {subsetor}"
     else:
         return ""
-
