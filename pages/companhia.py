949800438e5ec2cc95b59ab860460973c5b4a852
Fausto Stangler <fausto.stangler@gmail.com>
2023-10-12 19:15:35 -0300

    companhia

--- pages/companhia.py
+++ pages/companhia.py
@@ -12,34 +12,58 @@ import base64
 import gzip
 import io
 
-
+# ----- LAYOUT -----
 layout = html.Div([
+    # Storing data to be passed between callbacks
     dcc.Store(id='company-df', storage_type='session'), 
 
+    # Loading spinner that appears while callbacks are performed
     dcc.Loading(
         id="loading",
-        type="circle",  # "default", "cube", "circle", "cube", "dot", "cube", or "cube"
+        type="circle",
         children=[
+            # Main titles of the page
             html.H1("Análise Fundamentalista"),
             html.H2(id='company-segmento-title'), 
             html.H2(id='company-title'),
-            # Your content here...
+            
+            # Additional components like graphs, tables, etc.
             dcc.Graph(id='graph-ativos'),
+            # More contents here...
         ]
     )
 ])
 
+# ----- CALLBACKS -----
 @app.callback(
-    [Output('company-segmento-title', 'children'),
-     Output('company-title', 'children'),
-     Output('company-df', 'data')],  # Added output to store df
-    [Input('store-selected-company', 'data'),
-     Input('store-selected-setor', 'data'),
-     Input('store-selected-subsetor', 'data'),
-     Input('store-selected-segmento', 'data')]
+    [
+        Output('company-segmento-title', 'children'),
+        Output('company-title', 'children'),
+        Output('company-df', 'data')
+    ],  
+    [
+        Input('store-selected-company', 'data'),
+        Input('store-selected-setor', 'data'),
+        Input('store-selected-subsetor', 'data'),
+        Input('store-selected-segmento', 'data')
+    ]
 )
 def update_titles(stored_company, stored_setor, stored_subsetor, stored_segmento):
-    # If any of the stored data is None, prevent the update
+    """
+    Update titles and load data based on selected company, setor, subsetor, and segmento.
+
+    Parameters:
+    - stored_company (dict): Stored selected company data.
+    - stored_setor (dict): Stored selected setor data.
+    - stored_subsetor (dict): Stored selected subsetor data.
+    - stored_segmento (dict): Stored selected segmento data.
+
+    Returns:
+    - str: Updated segmento title.
+    - str: Updated company title.
+    - dict: Compressed and encoded company data.
+    """
+    # Check if the company data is available, if not, prevent the update
     if None in [stored_company]:
         raise exceptions.PreventUpdate
     
@@ -49,34 +73,30 @@ def update_titles(stored_company, stored_setor, stored_subsetor, stored_segmento
     subsetor = stored_subsetor.get('subsetor', '')
     segmento = stored_segmento.get('segmento', '')
     
-    # Construct the titles
-    # Load the DataFrame when a company is selected
+    # Initialize the titles and dataframe
     df = pd.DataFrame()
+    segmento_title = ''
+    company_title = ''
+    
+    # Load data and construct titles when a company is selected
     if companhia:
         # Construct the path to the file
         file = os.path.join(f'{b3.app_folder}company/{companhia}')
         
-        # Check if the file exists
+        # Check if the file exists and load the data
         if os.path.exists(file+'.pkl'):
-            # Load the DataFrame
             df = run.load_pkl(file)
-            # Now df contains the data and can be used for further processing
-            print(len(df), companhia)
         else:
             print(f"No data file found for company: {companhia}")
 
+        # Construct the titles
         segmento_title = f"{setor} - {subsetor} - {segmento}"
         company_title = companhia
-    else:
-        segmento_title = ''
-        company_title = ''
 
-    # stored_df = df.to_dict(orient='records')
-    # Compress and encode data
+    # Compress and encode data for efficient storage
     buffer = io.BytesIO()
     with gzip.GzipFile(fileobj=buffer, mode='w') as f:
         df.to_parquet(f)
     compressed_data = base64.b64encode(buffer.getvalue()).decode('utf-8')
-
 
     return segmento_title, company_title, compressed_data
