7866dd76f5febb5e85b781f3388cd18efb960bed
Fausto Stangler <fausto.stangler@gmail.com>
2023-10-12 19:00:40 -0300

    index

--- index.py
+++ index.py
@@ -11,33 +11,42 @@ import assets.functions as run
 
 import pandas as pd
 
+# ----- LAYOUT -----
 app.layout = html.Div([
+    # Include navigation layout
     nav.layout, 
+    # Horizontal line for separation
     html.Hr(),
-    dcc.Location(id='url', refresh=False),  
-
+    # URL component to track the current pathname
+    dcc.Location(id='url', refresh=False),
+    # Data stores to maintain selections across pages
     dcc.Store(id='store-selected-setor', storage_type='session'),
     dcc.Store(id='store-selected-subsetor', storage_type='session'),
     dcc.Store(id='store-selected-segmento', storage_type='session'),
     dcc.Store(id='store-selected-company', storage_type='session'),
-
+    # Main layout divided into two columns: Sidebar and Content
     dbc.Row([
         dbc.Col([
             dbc.Row([
-            sidebar.layout
+                sidebar.layout  # Include sidebar layout
             ], id='sidebar'), 
-            dbc.Row([
-            # nav.layout
-            ], id='nav')
-            
-        ], width=2),
-        dbc.Col(id="content", width=10),
+            dbc.Row([], id='nav')  # Placeholder for additional navigation (if needed)
+        ], width=2),  # Sidebar takes up 2 out of 12 grid columns
+        dbc.Col(id="content", width=10),  # Content takes up the remaining 10 columns
     ])
 ])
 
+# ----- CALLBACKS -----
+# Define callback to render page content based on URL
 @app.callback(Output("content", "children"), 
               [Input("url", "pathname")])
 def render_page_content(pathname):
+    """
+    Determine which layout to display based on the current URL/pathname.
+    If the pathname matches a predefined route, return the corresponding page layout.
+    If not, return a 404 "Not found" message.
+    """
+    # Define page routing
     if pathname in ['/', "/home"]:
         return home.layout
     elif pathname == "/setor":
@@ -48,35 +57,16 @@ def render_page_content(pathname):
         return segmento.layout
     elif pathname == "/companhia":
         return companhia.layout
+    # If pathname does not match any predefined route, display a 404 message
+    else:
+        return dbc.Card([
+            dbc.CardBody([
+                html.H1("404: Not found", className="text-danger"),
+                html.Hr(),
+                html.P(f"The pathname '{pathname}' was not recognised..."),
+            ])
+        ], className='mt-5')
 
-    return dbc.Card([
-        dbc.CardBody([
-            html.H1("404: Not found", className="text-danger"),
-            html.Hr(),
-            html.P(f"The pathname '{pathname}' was not recognised..."),
-        ])
-    ], className='mt-5')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Run the app
 if __name__ == "__main__":
     app.run_server(port=8051, debug=True)
