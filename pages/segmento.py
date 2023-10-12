3e4d3bc96d29b2b9ed376b3d4ef0fee7cb446d6f
Fausto Stangler <fausto.stangler@gmail.com>
2023-10-12 19:13:09 -0300

    segmento

--- pages/segmento.py
+++ pages/segmento.py
@@ -3,28 +3,51 @@ from dash.dependencies import Input, Output
 
 from app import app
 
+# ----- LAYOUT -----
 layout = html.Div([
+    # Main title of the page
     html.H1("Análise Fundamentalista"),
+    
+    # Subtitle that will be updated based on user selection
     html.H2(id='segmento-title'),
-    # Your contents here...
+    
+    # Additional contents might go here, like graphs, tables, etc.
+    # Uncomment and adjust the following line as per your requirements.
     # dcc.Graph(id='graph-ativos'),
+    
+    # More contents here...
 ])
 
-# Callbacks for this page...
-
+# ----- CALLBACKS -----
 @app.callback(
-    Output('segmento-title', 'children'),
-    [Input('store-selected-setor', 'data'),
-     Input('store-selected-subsetor', 'data'),
-     Input('store-selected-segmento', 'data')]
+    Output('segmento-title', 'children'),  # Target component to display the updated data
+    [Input('store-selected-setor', 'data'),  # Source component which triggers the callback
+     Input('store-selected-subsetor', 'data'),  # Another source component for the callback
+     Input('store-selected-segmento', 'data')]  # Another source component for the callback
 )
 def update_segmento_content(stored_setor, stored_subsetor, stored_segmento):
+    """
+    Update the subtitle based on selected 'setor', 'subsetor', and 'segmento'.
+
+    Parameters:
+    - stored_setor (dict): Stored data for selected 'setor'.
+    - stored_subsetor (dict): Stored data for selected 'subsetor'.
+    - stored_segmento (dict): Stored data for selected 'segmento'.
+
+    Returns:
+    - str: Updated subtitle string containing 'setor', 'subsetor', and 'segmento'.
+    """
+    # Check if there's any data stored, if not, prevent the update
     if stored_setor is None or stored_subsetor is None or stored_segmento is None:
         raise exceptions.PreventUpdate
     
+    # Retrieve 'setor', 'subsetor', and 'segmento' values from the stored data
     setor = stored_setor.get('setor', '')
     subsetor = stored_subsetor.get('subsetor', '')
-    segmento = stored_segmento.get('segmento', '')  # Fixed this line
+    segmento = stored_segmento.get('segmento', '')
+    
+    # Update the title based on the selected 'setor', 'subsetor', and 'segmento'
+    # If 'segmento' is available, display it along with 'setor' and 'subsetor' as the subtitle; otherwise, display an empty string
     if segmento:
         return f"{setor} - {subsetor} - {segmento}"
     else:
