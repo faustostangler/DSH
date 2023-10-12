5e38e4e537cf7388a73faf2ab4cb8161fec1c4d3
Fausto Stangler <fausto.stangler@gmail.com>
2023-10-12 19:04:19 -0300

    home

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-import dash
-import dash_bootstrap_components as dbc
+# Import necessary libraries
+import dash  # Import the Dash framework
+import dash_bootstrap_components as dbc  # Import Bootstrap components
 
-import importlib
-import os
+# Note: The below imports are placeholders and will depend on your project's structure.
+# import importlib  # Import the Python standard module for importing modules programmatically 
+# import os  # Import the Python standard module to interact with the operating system
 
+# Initialize the Dash app
+# `suppress_callback_exceptions=True` allows defining callbacks that are not in the initial layout of the app
+# `external_stylesheets` parameter is utilized to apply external styles to the app, here using Bootstrap's theme.
 app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# `server` attribute is an instance of Flask, which can be used to run the app
+# It can also be utilized to serve the app in production using a WSGI server like gunicorn
 server = app.server
--- pages/home.py
+++ pages/home.py
@@ -1,30 +1,37 @@
 from dash import html, dcc
 from dash.dependencies import Input, Output
-
 from app import app
-
 import assets.helper as b3
 import assets.functions as run
-
 import pandas as pd
 
+# ----- LAYOUT -----
+# Define the layout for a specific page in the app
 layout = html.Div([
+    
+    # Main title
     html.H1("Análise Fundamentalista"),
+
+    # Space for visual separation
     html.H2(" "),
+
+    # Loading component to provide a loading spinner for background data loading
     dcc.Loading(
         id="loading",
         type="circle",
         children=[
+            # Interval component triggers the data loading function at specified intervals
             dcc.Interval(
                 id='interval-load-data',
-                interval=1*1000,  # in milliseconds
+                interval=1*1000,  # 1 second interval
                 n_intervals=0,
-                max_intervals=1  # stops the interval after 1 update
+                max_intervals=1  # Stops after 1st interval to prevent continuous loading
             ),
+            
+            # Placeholder or component to display a data stat during loading
             html.H4(id='indicator-total-companies'),
-            # ... other components to display other stats ...
         ]
     ),
 ])
 
-
+# ----- CALLBACKS -----
